af95b8099c11c7ffd5a4d477a2a59450aa3f39ce
jcool-kfujikawa <k.fujikawa@j-cool.fm>
2023-07-07 12:01:38 +0900

    update-0707

--- index.php
+++ index.php
@@ -3,13 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>WordPressのテンプレート</title>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
-  
   <?php wp_head(); ?>
 </head>
 <body>
   <p class="test">WordPressのテンプレートです。</p>
-<?php wp_footer(); ?>
+  <?php wp_footer(); ?>
 </body>
 </html>
