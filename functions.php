87a8697538ecceb89d02b6e7e4c6b9fb252417d6
jcool-kfujikawa <k.fujikawa@j-cool.fm>
2024-09-06 12:38:56 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,3 +10,15 @@
   function is_local_environment() {
     return (defined('WP_ENVIRONMENT_TYPE') && WP_ENVIRONMENT_TYPE === 'local');
   }
+
+  // 投稿者アーカイブを無効化する
+  add_filter( 'author_rewrite_rules', '__return_empty_array' );
+
+  function disable_author_archive() {
+    if( isset($_GET['author']) || preg_match('#/author/.+#', $_SERVER['REQUEST_URI']) ){
+      wp_redirect( home_url() );
+      exit;
+    }
+  }
+
+  add_action('init', 'disable_author_archive');
